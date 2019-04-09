b9d8b5e5d22379ba01b2a8bd0ba569d532c96520
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2019-04-09 18:43:34 -0400

    Create cyrillic.js

---
+++ cyrillic.js
@@ -0,0 +1,751 @@
+var a = document.getElementById("myAudioA");
+var b = document.getElementById("myAudioB");
+var c = document.getElementById("myAudioC");
+var d = document.getElementById("myAudioD");
+var e = document.getElementById("myAudioE");
+var f = document.getElementById("myAudioF");
+var g = document.getElementById("myAudioG");
+var h = document.getElementById("myAudioH");
+var i = document.getElementById("myAudioI");
+var j = document.getElementById("myAudioJ");
+var k = document.getElementById("myAudioK");
+var l = document.getElementById("myAudioL");
+var m = document.getElementById("myAudioM");
+var n = document.getElementById("myAudioN");
+var o = document.getElementById("myAudioO");
+var p = document.getElementById("myAudioP");
+var q = document.getElementById("myAudioQ");
+var r = document.getElementById("myAudioR");
+var s = document.getElementById("myAudioS");
+var t = document.getElementById("myAudioT");
+var u = document.getElementById("myAudioU");
+var v = document.getElementById("myAudioV");
+var w = document.getElementById("myAudioW");
+var x = document.getElementById("myAudioX");
+var y = document.getElementById("myAudioY");
+var z = document.getElementById("myAudioZ");
+var xy = document.getElementById("myAudioXY");
+var yx = document.getElementById("myAudioYX");
+var zx = document.getElementById("myAudioZX");
+var xz = document.getElementById("myAudioXZ");
+var zy = document.getElementById("myAudioZY");
+var yz = document.getElementById("myAudioYZ");
+
+function playAudioA() {
+  a.play();
+}
+
+function playSentencesA() {
+  setTimeout(function() {
+    $("#firsta").append("<span style='color:blue'> и</span>кона́");
+  }, 1000);
+  setTimeout(function() {
+    $("#seconda").append(" л<span style='color:blue'>и</span>ст");
+  }, 2300);
+  setTimeout(function() {
+    $("#thirda").append(" рук<span style='color:blue'>и</span>");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourtha").append("");
+  }, 5200);
+  setTimeout(function() {
+    $("#fiftha").append(" ");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixtha").append(" ");
+  }, 7800);
+}
+
+function playAudioB() {
+  b.play();
+}
+
+function playSentencesB() {
+  setTimeout(function() {
+    $("#firstb").append("<span style='color:blue'> й</span>о́га");
+  }, 1000);
+  setTimeout(function() {
+    $("#secondb").append(" трамва<span style='color:blue'>й</span>");
+  }, 2300);
+  setTimeout(function() {
+    $("#thirdb").append(" новы<span style='color:blue'>й</span>");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourthb").append(" во<span style='color:blue'>й</span>");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifthb").append(" стро<span style='color:blue'>й</span>");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthb").append(" ду<span style='color:blue'>й</span>");
+  }, 7800);
+}
+
+function playAudioC() {
+  c.play();
+}
+
+function playSentencesC() {
+  setTimeout(function() {
+    $("#firstc").append("<span style='color:blue'> в</span>ы");
+  }, 1000);
+  setTimeout(function() {
+    $("#secondc").append("<span style='color:blue'> в</span>рал");
+  }, 2000);
+  setTimeout(function() {
+    $("#thirdc").append(" со<span style='color:blue'>в</span>ы");
+  }, 3000);
+  setTimeout(function() {
+    $("#fourthc").append(" уда<span style='color:blue'>в</span>ы");
+  }, 4000);
+  setTimeout(function() {
+    $("#ашаерc").append("");
+  }, 7800);
+}
+
+function playAudioD() {
+  d.play();
+}
+
+function playSentencesD() {
+  setTimeout(function() {
+    $("#firstd").append("<span style='color:blue'> б</span>ыл");
+  }, 600);
+  setTimeout(function() {
+    $("#secondd").append("<span style='color:blue'> б</span>аран");
+  }, 1800);
+  setTimeout(function() {
+    $("#thirdd").append(" гу<span style='color:blue'>б</span>ы");
+  }, 2800);
+  setTimeout(function() {
+    $("#fourthd").append(" та<span style='color:blue'>б</span>ло");
+  }, 3800);
+  setTimeout(function() {
+    $("#fifthd").append(" ");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthd").append("");
+  }, 7800);
+}
+
+function playAudioE() {
+  e.play();
+}
+
+function playSentencesE() {
+  setTimeout(function() {
+    $("#firste").append("<span style='color:blue'> ф</span>аза");
+  }, 1000);
+  setTimeout(function() {
+    $("#seconde").append("<span style='color:blue'> ф</span>орма");
+  }, 2000);
+  setTimeout(function() {
+    $("#thirde").append("<span style='color:blue'> ф</span>унт");
+  }, 3000);
+  setTimeout(function() {
+    $("#fourthe").append(" гра<span style='color:blue'>ф</span>");
+  }, 4000);
+  setTimeout(function() {
+    $("#fifthe").append("");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthe").append("");
+  }, 7800);
+}
+
+function playAudioF() {
+  f.play();
+}
+
+function playSentencesF() {
+  setTimeout(function() {
+    $("#firstf").append("<span style='color:blue'> р</span>ука");
+  }, 1000);
+  setTimeout(function() {
+    $("#secondf").append("<span style='color:blue'> р</span>ана");
+  }, 2300);
+  setTimeout(function() {
+    $("#thirdf").append(" д<span style='color:blue'>р</span>ама");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourthf").append(" да<span style='color:blue'>р</span>");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifthf").append("");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthf").append("");
+  }, 7800);
+}
+
+function playAudioG() {
+  g.play();
+}
+
+function playSentencesG() {
+  setTimeout(function() {
+    $("#firstg").append(
+      "<span style='color:blue'> п</span>а<span style='color:blue'>п</span>а"
+    );
+  }, 800);
+  setTimeout(function() {
+    $("#secondg").append("<span style='color:blue'> п</span>роза");
+  }, 1900);
+  setTimeout(function() {
+    $("#thirdg").append(" ла<span style='color:blue'>п</span>а");
+  }, 3000);
+  setTimeout(function() {
+    $("#fourthg").append(" су<span style='color:blue'>п</span>");
+  }, 4000);
+  setTimeout(function() {
+    $("#fifthg").append(" ");
+  }, 4000);
+  setTimeout(function() {
+    $("#sixthg").append(" ");
+  }, 7800);
+}
+
+function playAudioH() {
+  h.play();
+}
+
+function playSentencesH() {
+  setTimeout(function() {
+    $("#firsth").append("<span style='color:blue'> г</span>оды");
+  }, 700);
+  setTimeout(function() {
+    $("#secondh").append(" ду<span style='color:blue'>г</span>а");
+  }, 2000);
+  setTimeout(function() {
+    $("#thirdh").append(" у<span style='color:blue'>г</span>лы");
+  }, 3000);
+  setTimeout(function() {
+    $("#fourthh").append(" у<span style='color:blue'>г</span>он");
+  }, 4000);
+  setTimeout(function() {
+    $("#fifthh").append("");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthh").append(" ");
+  }, 7800);
+}
+
+function playAudioI() {
+  i.play();
+}
+
+function playSentencesI() {
+  setTimeout(function() {
+    $("#firsti").append("<span style='color:blue'> д</span>а");
+  }, 700);
+  setTimeout(function() {
+    $("#secondi").append("<span style='color:blue'> д</span>о");
+  }, 2000);
+  setTimeout(function() {
+    $("#thirdi").append("<span style='color:blue'> д</span>ата");
+  }, 3000);
+  setTimeout(function() {
+    $("#fourthi").append(" ту<span style='color:blue'>д</span>а");
+  }, 4000);
+  setTimeout(function() {
+    $("#fifthi").append("");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthi").append(" ");
+  }, 7800);
+}
+
+function playAudioJ() {
+  j.play();
+}
+
+function playSentencesJ() {
+  setTimeout(function() {
+    $("#firstj").append("<span style='color:blue'> л</span>ада");
+  }, 1000);
+  setTimeout(function() {
+    $("#secondj").append(" ту<span style='color:blue'>л</span>а");
+  }, 2300);
+  setTimeout(function() {
+    $("#thirdj").append(" мы<span style='color:blue'>л</span>");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourthj").append(" ду<span style='color:blue'>л</span>");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifthj").append("");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthj").append("");
+  }, 7800);
+}
+
+function playAudioK() {
+  k.play();
+}
+
+function playSentencesK() {
+  setTimeout(function() {
+    $("#firstk").append(" <span style='color:blue'>к</span>уда");
+  }, 800);
+  setTimeout(function() {
+    $("#secondk").append(" а<span style='color:blue'>к</span>ула");
+  }, 2000);
+  setTimeout(function() {
+    $("#thirdk").append(" ата<span style='color:blue'>к</span>а");
+  }, 3200);
+  setTimeout(function() {
+    $("#fourthk").append("о<span style='color:blue'>к</span>на");
+  }, 4000);
+  setTimeout(function() {
+    $("#fifthk").append(" ");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthk").append(" ");
+  }, 7800);
+}
+
+function playAudioL() {
+  l.play();
+}
+
+function playSentencesL() {
+  setTimeout(function() {
+    $("#firstl").append("<span style='color:blue'> м</span>ы");
+  }, 1000);
+  setTimeout(function() {
+    $("#secondl").append(
+      "<span style='color:blue'> м</span>а<span style='color:blue'>м</span>а"
+    );
+  }, 2300);
+  setTimeout(function() {
+    $("#thirdl").append(" да<span style='color:blue'>м</span>а");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourthl").append(" до<span style='color:blue'>м</span>");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifthl").append(" ");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthl").append(" ");
+  }, 7800);
+}
+
+function playAudioM() {
+  m.play();
+}
+
+function playSentencesM() {
+  setTimeout(function() {
+    $("#firstm").append("<span style='color:blue'> н</span>ота");
+  }, 1000);
+  setTimeout(function() {
+    $("#secondm").append("<span style='color:blue'> н</span>ыла");
+  }, 2300);
+  setTimeout(function() {
+    $("#thirdm").append(" лу<span style='color:blue'>н</span>а");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourthm").append(" до<span style='color:blue'>н</span>");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifthm").append("");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthm").append(" ");
+  }, 7800);
+}
+
+function playAudioN() {
+  n.play();
+}
+
+function playSentencesN() {
+  setTimeout(function() {
+    $("#firstn").append("<span style='color:blue'> х</span>ата");
+  }, 1600);
+  setTimeout(function() {
+    $("#secondn").append("<span style='color:blue'> х</span>алат");
+  }, 2300);
+  setTimeout(function() {
+    $("#thirdn").append(" у<span style='color:blue'>х</span>а");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourthn").append(" ма<span style='color:blue'>х</span>ал");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifthn").append(" ");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthn").append(" ");
+  }, 7800);
+}
+
+function playAudioO() {
+  o.play();
+}
+
+function playSentencesO() {
+  setTimeout(function() {
+    $("#firsto").append(" <span style='color:blue'> з</span>она");
+  }, 800);
+  setTimeout(function() {
+    $("#secondo").append("<span style='color:blue'> з</span>ал");
+  }, 1900);
+  setTimeout(function() {
+    $("#thirdo").append(" му<span style='color:blue'>з</span>ыка");
+  }, 3000);
+  setTimeout(function() {
+    $("#fourtho").append(" ко<span style='color:blue'>з</span>ы");
+  }, 4000);
+  setTimeout(function() {
+    $("#fiftho").append(" ");
+  }, 4000);
+  setTimeout(function() {
+    $("#sixtho").append(" ");
+  }, 7800);
+}
+
+function playAudioP() {
+  p.play();
+}
+
+function playSentencesP() {
+  setTimeout(function() {
+    $("#firstp").append(" к<span style='color:blue'>а</span>к");
+  }, 600);
+  setTimeout(function() {
+    $("#secondp").append(' "how"');
+  }, 1700);
+  setTimeout(function() {
+    $("#thirdp").append(" т<span style='color:blue'>а</span>к");
+  }, 2500);
+  setTimeout(function() {
+    $("#fourthp").append(' "so"');
+  }, 3800);
+  setTimeout(function() {
+    $("#fifthp").append(" т<span style='color:blue'>а</span>м");
+  }, 4950);
+  setTimeout(function() {
+    $("#sixthp").append('"there"');
+  }, 6000);
+}
+
+function playAudioQ() {
+  q.play();
+}
+
+function playSentencesQ() {
+  setTimeout(function() {
+    $("#firstq").append(" к<span style='color:blue'>о</span>т");
+  }, 1000);
+  setTimeout(function() {
+    $("#secondq").append(' "cat"');
+  }, 2000);
+  setTimeout(function() {
+    $("#thirdq").append(" т<span style='color:blue'>о</span>к");
+  }, 3000);
+  setTimeout(function() {
+    $("#fourthq").append(' "current"');
+  }, 4000);
+  setTimeout(function() {
+    $("#fifthq").append(" кт<span style='color:blue'>о</span>");
+  }, 5000);
+  setTimeout(function() {
+    $("#sixthq").append(' "who"');
+  }, 6000);
+}
+
+function playAudioR() {
+  r.play();
+}
+
+function playSentencesR() {
+  setTimeout(function() {
+    $("#firstr").append(" к<span style='color:blue'>у</span>м");
+  }, 1000);
+  setTimeout(function() {
+    $("#secondr").append(" т<span style='color:blue'>у</span>т");
+  }, 2300);
+  setTimeout(function() {
+    $("#thirdr").append(" м<span style='color:blue'>у</span>ка");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourthr").append(" ");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifthr").append(" ");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthr").append(" ");
+  }, 7800);
+}
+
+function playAudioS() {
+  s.play();
+}
+
+function playSentencesS() {
+  setTimeout(function() {
+    $("#firsts").append("<span style='color:blue'> э</span>та");
+  }, 800);
+  setTimeout(function() {
+    $("#seconds").append("<span style='color:blue'> э</span>мма");
+  }, 2000);
+  setTimeout(function() {
+    $("#thirds").append(" ");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourths").append(" ");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifths").append(" ");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixths").append(" ");
+  }, 7800);
+}
+
+function playAudioT() {
+  t.play();
+}
+
+function playSentencesT() {
+  setTimeout(function() {
+    $("#firstt").append(" т<span style='color:blue'>ы</span>");
+  }, 500);
+  setTimeout(function() {
+    $("#secondt").append(" м<span style='color:blue'>ы");
+  }, 1500);
+  setTimeout(function() {
+    $("#thirdt").append(
+      " м<span style='color:blue'>ы</span>т<span style='color:blue'>ы</span>"
+    );
+  }, 2500);
+  setTimeout(function() {
+    $("#fourtht").append(" ");
+  }, 5200);
+  setTimeout(function() {
+    $("#fiftht").append("");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixtht").append(" ");
+  }, 7800);
+}
+
+function playAudioU() {
+  u.play();
+}
+
+function playSentencesU() {
+  setTimeout(function() {
+    $("#firstu").append("<span style='color:blue'> я</span>ма");
+  }, 2000);
+  setTimeout(function() {
+    $("#secondu").append("<span style='color:blue'> я</span>сли");
+  }, 3000);
+  setTimeout(function() {
+    $("#thirdu").append("<span style='color:blue'> я</span>дра");
+  }, 4000);
+  setTimeout(function() {
+    $("#fourthu").append(" ");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifthu").append(" ");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthu").append(" ");
+  }, 7800);
+}
+
+function playAudioV() {
+  v.play();
+}
+
+function playSentencesV() {
+  setTimeout(function() {
+    $("#firstv").append("<span style='color:blue'> ю</span>ный");
+  }, 1000);
+  setTimeout(function() {
+    $("#secondv").append(" <span style='color:blue'> ю</span>рий");
+  }, 2300);
+  setTimeout(function() {
+    $("#thirdv").append("<span style='color:blue'> ю</span>билей");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourthv").append("<span style='color:blue'> ю</span>ла");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifthv").append("");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthv").append("");
+  }, 7800);
+}
+
+function playAudioW() {
+  w.play();
+}
+
+function playSentencesW() {
+  setTimeout(function() {
+    $("#firstw").append("<span style='color:blue'> е</span>ва");
+  }, 1000);
+  setTimeout(function() {
+    $("#secondw").append("<span style='color:blue'> е</span>ла");
+  }, 2300);
+  setTimeout(function() {
+    $("#thirdw").append("<span style='color:blue'> е</span>ду");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourthw").append("<span style='color:blue'> е</span>сли");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifthw").append("");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthw").append(" ");
+  }, 7800);
+}
+
+function playAudioX() {
+  x.play();
+}
+
+function playSentencesX() {
+  setTimeout(function() {
+    $("#firstx").append("<span style='color:blue'> Ё</span>мкий");
+  }, 1000);
+  setTimeout(function() {
+    $("#secondx").append("<span style='color:blue'> Ё</span>лка");
+  }, 2300);
+  setTimeout(function() {
+    $("#thirdx").append("<span style='color:blue'> Ё</span>рзал");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourthx").append("");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifthx").append("");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthx").append(" ");
+  }, 7800);
+}
+
+function playAudioY() {
+  y.play();
+}
+
+function playSentencesY() {
+  setTimeout(function() {
+    $("#firsty").append("<span style='color:blue'> й</span>о́га");
+  }, 1000);
+  setTimeout(function() {
+    $("#secondy").append(" трамва<span style='color:blue'>й</span>");
+  }, 2300);
+  setTimeout(function() {
+    $("#thirdy").append(" новы<span style='color:blue'>й</span>");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourthy").append(" во<span style='color:blue'>й</span>");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifthy").append(" стро<span style='color:blue'>й</span>");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthy").append(" ду<span style='color:blue'>й</span>");
+  }, 7800);
+}
+
+function playAudioZ() {
+  z.play();
+}
+
+function playSentencesZ() {
+  setTimeout(function() {
+    $("#firstz").append("<span style='color:blue'> й</span>о́га");
+  }, 1000);
+  setTimeout(function() {
+    $("#secondz").append(" трамва<span style='color:blue'>й</span>");
+  }, 2300);
+  setTimeout(function() {
+    $("#thirdz").append(" новы<span style='color:blue'>й</span>");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourthz").append(" во<span style='color:blue'>й</span>");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifthz").append(" стро<span style='color:blue'>й</span>");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthz").append(" ду<span style='color:blue'>й</span>");
+  }, 7800);
+}
+
+function playAudioXY() {
+  xy.play();
+}
+
+function playSentencesXY() {
+  setTimeout(function() {
+    $("#firstxy").append("<span style='color:blue'> й</span>о́га");
+  }, 1000);
+  setTimeout(function() {
+    $("#secondxy").append(" трамва<span style='color:blue'>й</span>");
+  }, 2300);
+  setTimeout(function() {
+    $("#thirdxy").append(" новы<span style='color:blue'>й</span>");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourthxy").append(" во<span style='color:blue'>й</span>");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifthxy").append(" стро<span style='color:blue'>й</span>");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthxy").append(" ду<span style='color:blue'>й</span>");
+  }, 7800);
+}
+
+function playAudioYX() {
+  yx.play();
+}
+
+function playSentencesB() {
+  setTimeout(function() {
+    $("#firstb").append("<span style='color:blue'> й</span>о́га");
+  }, 1000);
+  setTimeout(function() {
+    $("#secondb").append(" трамва<span style='color:blue'>й</span>");
+  }, 2300);
+  setTimeout(function() {
+    $("#thirdb").append(" новы<span style='color:blue'>й</span>");
+  }, 3800);
+  setTimeout(function() {
+    $("#fourthb").append(" во<span style='color:blue'>й</span>");
+  }, 5200);
+  setTimeout(function() {
+    $("#fifthb").append(" стро<span style='color:blue'>й</span>");
+  }, 6300);
+  setTimeout(function() {
+    $("#sixthb").append(" ду<span style='color:blue'>й</span>");
+  }, 7800);
+}
+
+function playAudioZX() {
+  zx.play();
+}
+
+function playAudioXZ() {
+  xz.play();
+}
+
+function playAudioYZ() {
+  yz.play();
+}
+
+function playAudioZY() {
+  zy.play();
+}
